1316e5e538515bc023281d36812873f2f51e180d
Cledson Ferreira <cledsonitgames@gmail.com>
2022-08-04 12:50:03 -0300

    View to read a recipe

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,15 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html', {
+        'name': 'Tales Silva'
+    }, status=200)
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', {
         'name': 'Tales Silva'
     }, status=200)
