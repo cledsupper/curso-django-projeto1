81e9a37f5fce362dc6ecff6af1f8f20daf3524f9
Cledson Ferreira <cledsonitgames@gmail.com>
2022-08-11 12:09:59 -0300

    New view for recipe details

--- recipes/views.py
+++ recipes/views.py
@@ -7,3 +7,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', {
         'name': 'Tales Silva'
     }, status=200)
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', {
+        'name': 'Tales Silva'
+    }, status=200)
