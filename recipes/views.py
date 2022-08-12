54a87906baf4a4cc15694cab0ca16ef593d7544d
Cledson Ferreira <cledsonitgames@gmail.com>
2022-08-12 16:00:50 -0300

    Sample data for recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 # Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html', {
-        'name': 'Tales Silva'
+        'recipes': [make_recipe() for _ in range(10)]
     }, status=200)
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', {
-        'name': 'Tales Silva'
+        'recipe': make_recipe()
     }, status=200)
